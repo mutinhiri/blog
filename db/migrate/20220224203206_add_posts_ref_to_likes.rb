410e4454d93dd49b867915fbcc4800e687d4f945
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-25 08:33:47 +0200

    Add changes requested by the code reviewer

--- db/migrate/20220224203206_add_posts_ref_to_likes.rb
+++ db/migrate/20220224203206_add_posts_ref_to_likes.rb
@@ -1,5 +1,5 @@
 class AddPostsRefToLikes < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :likes, null: true, foreign_key: true
+    add_reference :likes, :post, null: false, foreign_key: true
   end
 end
