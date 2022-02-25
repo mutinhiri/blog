08d40392277c042987a0542ebe810b57fd9c92c6
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-25 19:02:36 +0200

    Add cha=nges

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_25_063205) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_24_203441) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,14 +19,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_25_063205) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "post_id", null: false
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -36,25 +40,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_25_063205) do
     t.integer "likescounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "likes_id"
-    t.bigint "user_id", null: false
-    t.integer "comments_counter"
-    t.integer "likes_counter"
-    t.index ["likes_id"], name: "index_posts_on_likes_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.text "bio"
+    t.integer "postcounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "like_id"
-    t.integer "posts_counter"
-    t.index ["like_id"], name: "index_users_on_like_id"
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
-  add_foreign_key "posts", "likes", column: "likes_id"
-  add_foreign_key "posts", "users"
-  add_foreign_key "users", "likes"
+  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
