88f2eb8d77050c3716555a649e46556ab18f970c
mutinhiri <barnabmutinhiri@gmail.com>
2022-03-01 11:52:29 +0200

    Update posts method to inrement

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :user
   has_many :like
+
+  def recent_comments
+    comment.order('created_at Desc').limit(5)
+  end
+
+  after_save :update_posts_counter
+
+  def update_posts_counter
+    user.increment!(:posts_counter)
+  end
 end
