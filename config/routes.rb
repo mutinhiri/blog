55dc46f8886e837eaf27f1418cb85e74d0421574
mutinhiri <barnabmutinhiri@gmail.com>
2022-03-03 08:32:07 +0200

    Add changes to routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :users, only: %i[show, index] do
+  resources :users, only: %i[show index] do
     resources :posts, only: %i[show create new index]
   end
+
   root to: 'users#index'
 
   resources :posts do
-    resources :comments, only: [:create]
-    resources :likes, only: [:create]
+    resources :comments, only: %i[create new]
+    resources :likes, only: %i[create]
   end
 end
