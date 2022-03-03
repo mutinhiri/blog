aca3672b180c0ececc66560db7840d3fe1f75f1b
mutinhiri <barnabmutinhiri@gmail.com>
2022-03-03 07:31:50 +0200

    Add comments controller

---
+++ app/controllers/comments_conroller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.post_id = params[:post_id]
+
+    @comment.update_comments_counter
+    if @comment.save
+      redirect_to user_post_path(current_user.id, Post.find(params[:post_id]))
+    else
+      render :new
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
