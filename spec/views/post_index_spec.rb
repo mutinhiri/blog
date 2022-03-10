dfaae8231e92ea4f5646f80c32665b1d2f2d1afe
mutinhiri <barnabmutinhiri@gmail.com>
2022-03-10 11:27:16 +0200

    Fill in user email

--- spec/views/post_index_spec.rb
+++ spec/views/post_index_spec.rb
@@ -5,3 +5,4 @@ RSpec.describe 'Login', type: :feature do
         before(:each) do
             @user1= User.create(name: 'first', password: '12345', bio: 'first user test', email: 'first@first.com', confirmed_at: Time.now)
             visit root_path
+            fill_in placeholder: 'User Email', with: 'first@first.com'
