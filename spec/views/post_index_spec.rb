9a2c5dab193f82b8a3db8d72ac5c2428a49a0af0
mutinhiri <barnabmutinhiri@gmail.com>
2022-03-10 11:18:48 +0200

    Add changes

--- spec/views/post_index_spec.rb
+++ spec/views/post_index_spec.rb
@@ -1,1 +1,7 @@
 require 'rails_helper'
+
+RSpec.describe 'Login', type: :feature do
+    describe 'User' do
+        before(:each) do
+            @user1= User.create(name: 'first', password: '12345', bio: 'first user test', email: 'first@first.com', confirmed_at: Time.now)
+            visit root_path
