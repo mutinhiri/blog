b7b84a40c8cae912fd96b2875d761534ed32685b
mutinhiri <barnabmutinhiri@gmail.com>
2022-03-10 11:29:10 +0200

    Finish User mock data set

--- spec/views/post_index_spec.rb
+++ spec/views/post_index_spec.rb
@@ -6,3 +6,7 @@ RSpec.describe 'Login', type: :feature do
             @user1= User.create(name: 'first', password: '12345', bio: 'first user test', email: 'first@first.com', confirmed_at: Time.now)
             visit root_path
             fill_in placeholder: 'User Email', with: 'first@first.com'
+            fill_in placeholder: 'User Password', with '12345'
+            click_button 'Log in'
+
+            @post1 = Post.create
