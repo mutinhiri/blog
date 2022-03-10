260f35c7b7b2fa5128a16fd872b06253a2750d10
mutinhiri <barnabmutinhiri@gmail.com>
2022-03-10 11:40:39 +0200

    Add mock posts

--- spec/views/post_index_spec.rb
+++ spec/views/post_index_spec.rb
@@ -9,4 +9,5 @@ RSpec.describe 'Login', type: :feature do
             fill_in placeholder: 'User Password', with '12345'
             click_button 'Log in'
 
-            @post1 = Post.create
+            @post1 = Post.create(title: 'First Post', text: 'This is the first post', comments_counter: 0, likes_counter: 0, user: @user1)
+            @post
